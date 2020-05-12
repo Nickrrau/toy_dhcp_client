a90844fdb4f17336fc8aaf864563c03a8333e5ff
nrr <nickrrau@gmail.com>
2020-05-12 16:41:31 -0400

    Added some TODO's if I get to a file persisted XID and/or randomly generated XID

--- xid.go
+++ xid.go
@@ -1,1 +1,11 @@
 package main
+
+//TODO: Look for a config file
+
+//TODO: Generate XID or Copy from File
+
+//TODO: Random Generation for XID
+
+//TODO: Clear File of XID
+
+//TODO: Save XID to file
