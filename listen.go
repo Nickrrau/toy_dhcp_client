462461fa3343613db75f08a3b166f780e7dba8db
nrr <nickrrau@gmail.com>
2020-05-12 21:28:36 -0400

    Modified Client.listen() to run once, now let the client handle retrying in the Client.Run() method.

--- listen.go
+++ listen.go
@@ -1,45 +1,43 @@
 package toy_dhcp_client
 
 import (
-	"toy_dhcp_client/message"
 	"bufio"
 	"bytes"
 	"errors"
 	"fmt"
 	"net"
+	"toy_dhcp_client/message"
 )
 
-func (cl *DHCPClient) listen() (*message.DHCPMsg, error) {
-	listener, err := net.ListenUDP("udp", &net.UDPAddr{Port: 68}) //TODO: Set Timeout
+func (cl *Client) listen() (*message.DHCPMsg, error) {
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: dhcpPortReceive}) //TODO: Set Timeout
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	defer listener.Close()
-	for {
-		reader := bufio.NewReader(listener)
-		status := make([]byte, reader.Size())
-		_, err := reader.Read(status)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		switch cl.state {
-		case DHCP_CLIENT_DISCOVERING:
-			if message.MessageType(status[0]) == message.BOOT_REPLY && bytes.Compare(status[4:8], cl.xid) == 0 { //TODO: Move this check into parseMsg so that we can get rid of the duped code
-				return message.BytesToDHCPMsg(status)
-			} else {
-				return nil, errors.New("Incorrect Message Type, Ignoring") //TODO: Make this more informative
-			}
-		case DHCP_CLIENT_REQUESTING:
-			fmt.Println(status[0])
-			if message.MessageType(status[0]) == message.BOOT_REPLY && bytes.Compare(status[4:8], cl.xid) == 0 { //TODO: Move this check into parseMsg so that we can get rid of the duped code
-				return message.BytesToDHCPMsg(status)
-			} else {
-				return nil, errors.New("Incorrect Message Type, Ignoring") //TODO: Make this more informative
-			}
-		default:
-			continue
-		}
+	defer conn.Close()
 
+	reader := bufio.NewReader(conn)
+	buff := make([]byte, reader.Size())
+	_, err = reader.Read(buff)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	switch cl.state {
+	case DHCP_CLIENT_DISCOVERING:
+		if message.MessageType(buff[0]) == message.BOOT_REPLY && bytes.Compare(buff[4:8], cl.xid) == 0 { //TODO: Move this check into parseMsg so that we can get rid of the duped code
+			return message.BytesToDHCPMsg(buff)
+		} else {
+			return nil, errors.New("Incorrect Message Type, Ignoring") //TODO: Make this more informative
+		}
+	case DHCP_CLIENT_REQUESTING:
+		fmt.Println(buff[0])
+		if message.MessageType(buff[0]) == message.BOOT_REPLY && bytes.Compare(buff[4:8], cl.xid) == 0 { //TODO: Move this check into parseMsg so that we can get rid of the duped code
+			return message.BytesToDHCPMsg(buff)
+		} else {
+			return nil, errors.New("Incorrect Message Type, Ignoring") //TODO: Make this more informative
+		}
+	default:
+		return nil, nil
 	}
 }
