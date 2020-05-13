7f834ce3cfe5d5e3a65c0ce0547da13977dea995
nrr <nickrrau@gmail.com>
2020-05-12 23:04:17 -0400

    Added DHCP Message type pass in through options

--- message/message_type.go
+++ message/message_type.go
@@ -6,3 +6,14 @@ const (
 	BOOT_REQEUST MessageType = iota + 1
 	BOOT_REPLY
 )
+
+type DHCPMessageType byte
+
+const (
+	MSGTYPE_DISCOVER DHCPMessageType = iota + 1
+	MSGTYPE_OFFER
+	MSGTYPE_REQUEST
+	//TODO: 4?
+	MSGTYPE_ACK  DHCPMessageType = 5
+	MSGTYPE_NACK DHCPMessageType = 6
+)
