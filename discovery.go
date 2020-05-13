3370516804f5037bfebd990e363206753976d51b
nrr <nickrrau@gmail.com>
2020-05-12 21:29:35 -0400

    Fixed Defer location and moved broadcast address to a package level variable.

--- discovery.go
+++ discovery.go
@@ -6,13 +6,18 @@ import (
 	"toy_dhcp_client/message"
 )
 
-func (cl *DHCPClient) discover() error {
-	conn, err := net.Dial("udp", "255.255.255.255:67")
-	defer conn.Close()
+const (
+	broadcastAddr   = "255.255.255.255:67"
+	dhcpPortReceive = 68
+)
+
+func (cl *Client) discover() error {
+	conn, err := net.Dial("udp", broadcastAddr)
 	if err != nil {
 		fmt.Printf("Closing Connecting: %v\n", err)
 		return err
 	}
+	defer conn.Close()
 
 	msg := message.NewDiscoverMsg(cl.xid, cl.iface.HardwareAddr, cl.ops)
 	err = msg.WriteToConn(conn)
