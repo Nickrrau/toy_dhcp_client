0bd5c8cb4f19155acb125f2f1d2ac994807fddfe
Nick Rau <nickrrau@gmail.com>
2020-05-13 02:05:27 -0400

    Removed prints and added a timeout of 5 seconds to each attempt to listen for a connection

--- listen.go
+++ listen.go
@@ -4,24 +4,25 @@ import (
 	"bufio"
 	"bytes"
 	"errors"
-	"fmt"
 	"net"
+	"time"
 	"toy_dhcp_client/message"
 )
 
 //Client.listen() listens on port 68 for broadcasted messages, messages are checked for correct message type and xid before commiting to parsing the full bytes.
 func (cl *Client) listen() (*message.DHCPMsg, error) {
-	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: dhcpPortReceive}) //TODO: Set Timeout
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{Port: dhcpPortReceive})
 	if err != nil {
 		return nil, err
 	}
 	defer conn.Close()
 
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+
 	reader := bufio.NewReader(conn)
 	buff := make([]byte, reader.Size())
 	_, err = reader.Read(buff)
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 	switch cl.state {
@@ -32,7 +33,6 @@ func (cl *Client) listen() (*message.DHCPMsg, error) {
 			return nil, errors.New("Incorrect Message Type, Ignoring") //TODO: Make this more informative
 		}
 	case DHCP_CLIENT_REQUESTING:
-		fmt.Println(buff[0])
 		if message.MessageType(buff[0]) == message.BOOT_REPLY && bytes.Compare(buff[4:8], cl.xid) == 0 { //TODO: Move this check into parseMsg so that we can get rid of the duped code
 			return message.BytesToDHCPMsg(buff)
 		} else {
